0fa3df93542fa947f09cefef90d37447ccdb83c2
kennyballou <kballou@onyx.boisestate.edu>
2013-07-19 22:48:53 -0600

    Add top level slides build.py
    *   Add ability to build all slides at once

---
+++ slides/build.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""Slides build.py"""
+
+import os
+import xnt
+import xnt.build.tex
+
+slides = ['2013-02-15',
+          '2013-03-15',
+          '2013-03-20',
+          '2013-05-03',
+          '2013-06-29',
+          '2013-07-20',]
+
+@xnt.target
+def build():
+    """Build Slides"""
+    for slide in slides:
+        xnt.tasks.xntcall(os.path.join(slide, 'build.py'),
+                          targets=['build',])
+
+@xnt.target
+def default():
+    """Build Slides"""
+    build()
+    clean()
+
+@xnt.target
+def clean():
+    """Remove Generated Files"""
+    for slide in slides:
+        xnt.tasks.xntcall(os.path.join(slide, 'build.py'),
+                          targets=['clean',])
+
+@xnt.target
+def clean_pdf():
+    """Remove _all_ generated files (including PDF output)"""
+    for slide in slides:
+        xnt.tasks.xntcall(os.path.join(slide, 'build.py'),
+                          targets=['clean_pdf',])
