b029de243bbafddadeb72d296378bddc269cb253
kennyballou <kballou@onyx.boisestate.edu>
2013-07-31 20:59:45 -0600

    Add sc13 submission -- extended abstract

---
+++ tex/sc13/build.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Slides build.py"""
+
+import xnt
+import xnt.build.tex
+
+PROPERTIES = { "doc_name": "poster_abstract.tex", }
+
+@xnt.target
+def build():
+    """Build Slides"""
+    return xnt.build.tex.pdflatex(PROPERTIES["doc_name"])
+
+@xnt.target
+def default():
+    """Build Slides"""
+    build()
+    clean()
+
+@xnt.target
+def clean():
+    """Remove Generated Files"""
+    xnt.build.tex.clean(path="./")
+
+@xnt.target
+def clean_pdf():
+    """Remove _all_ generated files (including PDF output)"""
+    xnt.build.tex.clean(path="./", remove_pdf=True)
